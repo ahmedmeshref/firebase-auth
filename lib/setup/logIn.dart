57fc6bfb81f0f8843ddc1cd1cf96d2f07943109f
ahmedmeshref <a.meshref@alustudent.com>
2021-03-30 21:03:24 +0200

    build sign in form

--- lib/setup/logIn.dart
+++ lib/setup/logIn.dart
@@ -8,11 +8,46 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String _email, _password;
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sign in'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onSaved: (input) => _email = input,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              validator: (value) {
+                if (value.length < 6) {
+                  return 'Password should contain at least 6 chars';
+                }
+                return null;
+              },
+              onSaved: (input) => _password = input,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Sign in'),
+            )
+          ],
+        ),
       ),
     );
   }
